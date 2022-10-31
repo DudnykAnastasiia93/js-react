9b5df9faf8c0190750263a461c33c28ff3e16599
Nastya <example@gmail.com>
2022-10-31 18:32:00 +0200

    20lessons

--- js/script.js
+++ js/script.js
@@ -1,24 +1,86 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-        
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
-        
+// console.log(personalMovieDB);
+
+// const num = 50;
+
+// ( num === 50 ) ? console.log('ok!') : console.log('error');
+
+// свитч кейс
+
+// const num = 50;
+
+// switch(num) {
+//     case 49:
+//         console.log('no');
+//         break;
+//     case 100:
+//         console.log('no');
+//         break;
+//     case 50:
+//         console.log('yes');
+//         break;
+//     default:
+//         console.log('nonono');
+//         break;
+// }
+
+////////////////////////////////////////////
+
+// задачка елочка
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = "";
+// const length = 7;
+
+// for (let i = 0; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
+
+///////////////////////////////////////////////////////////////////////////
+
+// вложенные циклы и метки
+
+
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
 

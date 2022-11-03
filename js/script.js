9a22397d1e3fe41db845cc6df263e9bca777cfdd
Nastya <example@gmail.com>
2022-11-03 13:58:13 +0200

    22lessons

--- js/script.js
+++ js/script.js
@@ -84,3 +84,39 @@
 //     }
 // }
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
+        
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Слишком мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классный зритель');
+} else if(personalMovieDB.count >= 30)  {
+    console.log('Вы киноман');
+} else {
+    console.log('произошла ошибка');
+}
+
+console.log(personalMovieDB);

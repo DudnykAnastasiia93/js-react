ff450907ea52c7b80526c21e9a57eb817e11d312
Nastya <example@gmail.com>
2022-11-06 14:18:38 +0200

    30lessons

--- js/script.js
+++ js/script.js
@@ -84,6 +84,64 @@
 //     }
 // }
 
+
+/////////////////////////////////
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     }
+// };
+
+// let counter = 0;
+
+// for( let key in options ) {
+//     if(typeof(options[key]) === "object") {
+//         for(let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     }else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);
+
+
+/////////////////////////////////////////////
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('Test');
+//     }
+// };
+
+// options.makeTest();
+
+// console.log(Object.keys(options).length);
+
+// // деструктуризация
+
+// const {border, bg} = options.colors;
+
+// console.log(border);
+
+
+
+
 let numberOfFilms; 
 
 function start() {

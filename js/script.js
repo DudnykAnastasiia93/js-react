004852d792b881d100ba10a48cd9967cb82d3bd8
Nastya <example@gmail.com>
2022-12-21 13:59:33 +0200

    44lessons

--- js/script.js
+++ js/script.js
@@ -274,7 +274,6 @@
 
 // console.log(c1, c2, c3);
 
-
 /////////////////////////////////////задачки с собеседований
 
 // let x = 5;
@@ -282,7 +281,6 @@
 // alert(x++);
 
 // ответ 5
-
 
 ////////////
 
@@ -304,7 +302,6 @@
 
 /////////////
 
-
 // const res = [] + 1 + 2;
 
 // console.log(res);
@@ -317,17 +314,15 @@
 
 // Ответ 1 (string)
 
-
 // console.log(2 && 1 && null && 0 && undefined);
 
 // ответ null (оператор && всегда запинается на лжи, оператор || наоборот на правде)
 
-
 //////
 
-  // console.log(!!(1 && 2 ) == (1 && 2 ))
+// console.log(!!(1 && 2 ) == (1 && 2 ))
 
-  // Ответ false, выражения не равны, потому что !! переводит код в булиновое значение
+// Ответ false, выражения не равны, потому что !! переводит код в булиновое значение
 
 ///////////////
 
@@ -335,9 +330,7 @@
 
 // ответ 3
 
-
 ////////////////
-
 
 // const a = [1, 2, 3];
 // const b = [1, 2, 3];
@@ -368,29 +361,99 @@
 // for( let node of document.body.childNodes) {
 //   if (node.nodeName == '#text') {
 //     continue;
-//   }else {
+//   }
 //     console.log(node);
+//
+// }
+
+//////////////////////////////////////////// рекурсия (вызов функции внутри ее же)
+
+// function pow(x, n) {
+//   let result = 1;
+
+//   for( let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+// сверху функция возведения числа в степень, ниже то же самое, только с помощью рекурсии
+
+// function pow(x, n) {
+//   if(n === 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n-1);
 //   }
 // }
 
+// pow(2, 1) // 2
+// pow(2, 2) // 4
+// pow(2, 3) // 8
+// pow(2, 4) // 16
 
-////////////////////////////////////////////
+//////////////////////////////////задачка с подсчетом студентов и прогрессом
+
+let students = {
+  js: [
+    {
+      name: "John",
+      progress: 100,
+    },
+    {
+      name: "Ivan",
+      progress: 60,
+    },
+  ],
+  html: {
+    basic: [
+      {
+        name: "Katya",
+        progress: 70,
+      },
+      {
+        name: "Petya",
+        progress: 50,
+      },
+    ],
+    pro: [
+      {
+        name: "Vasya",
+        progress: 75,
+      },
+      {
+        name: "Olya",
+        progress: 40,
+      },
+    ]
+  }
+};
 
 
+function getTotalProgressByIteration(data) {
+  let total = 0;
+  let students = 0;
 
+  for(let course of Object.values(data)) {
+    if(Array.isArray(course)){
+      students += course.length
+      for(let i = 0; i < course.length; i++) {
+        total += course[i].progress;
+      }
+    }else {
+      for(let subCourse of Object.values(course)) {
+        students += subCourse.length;
+        for(let i = 0; i < subCourse.length; i++) {
+          total += subCourse[i].progress;
+        }
+      }
+    }
+  }
+  return total / students;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(getTotalProgressByIteration(students));
 
 // const personalMovieDB = {
 //   count: 0,
@@ -463,7 +526,6 @@
 //     //     personalMovieDB.genres[i - 1] = genre;
 //     //   }
 //     // }
-
 
 //    ///////////////////////////////////////////////////////////////////////////////то же самое немного по-другому
 
